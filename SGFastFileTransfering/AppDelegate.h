883d87356b96f397223f28ca9faa845b59d53731
Aban <aban@Macs-iMac.local>
2018-07-27 20:24:33 +0430

    Project Initiated

---
+++ SGFastFileTransfering/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SGFastFileTransfering
+//
+//  Created by Aban on 7/27/18.
+//  Copyright © 2018 Aban. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SGFastFileTransfering/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SGFastFileTransfering
+//
+//  Created by Aban on 7/27/18.
+//  Copyright © 2018 Aban. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
